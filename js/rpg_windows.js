f772ffbe414672e90881aafd73f9a7ee3fe6514f
shibucat <shibucat@users.noreply.github.com>
2021-04-17 18:28:46 -0400

    4-17-21

--- js/rpg_windows.js
+++ js/rpg_windows.js
@@ -32,8 +32,9 @@ Window_Base._iconHeight = 32;
 Window_Base._faceWidth  = 144;
 Window_Base._faceHeight = 144;
 
+//Title line height default is 26
 Window_Base.prototype.lineHeight = function() {
-    return 36;
+    return 46;
 };
 
 Window_Base.prototype.standardFontFace = function() {
@@ -45,17 +46,17 @@ Window_Base.prototype.standardFontFace = function() {
         return 'GameFont';
     }
 };
-
+//Ttile font size deafult is 28
 Window_Base.prototype.standardFontSize = function() {
-    return 28;
+    return 48;
 };
 
 Window_Base.prototype.standardPadding = function() {
-    return 18;
+    return 28;
 };
 
 Window_Base.prototype.textPadding = function() {
-    return 6;
+    return 16;
 };
 
 Window_Base.prototype.standardBackOpacity = function() {
@@ -3878,7 +3879,7 @@ Window_ChoiceList.prototype.windowWidth = function() {
     var width = this.maxChoiceWidth() + this.padding * 2;
     return Math.min(width, Graphics.boxWidth);
 };
-
+//♪
 Window_ChoiceList.prototype.numVisibleRows = function() {
     var messageY = this._messageWindow.y;
     var messageHeight = this._messageWindow.height;
@@ -5741,9 +5742,9 @@ Window_TitleCommand._lastCommandSymbol = null;
 Window_TitleCommand.initCommandPosition = function() {
     this._lastCommandSymbol = null;
 };
-
+//Width of the title window box
 Window_TitleCommand.prototype.windowWidth = function() {
-    return 240;
+    return 440;
 };
 
 Window_TitleCommand.prototype.updatePlacement = function() {
@@ -5752,7 +5753,7 @@ Window_TitleCommand.prototype.updatePlacement = function() {
 };
 
 Window_TitleCommand.prototype.makeCommandList = function() {
-    this.addCommand(TextManager.newGame,   'newGame');
+     this.addCommand(TextManager.newGame,   'newGame');
 //    this.addCommand(TextManager.continue_, 'continue', this.isContinueEnabled());
 //    this.addCommand(TextManager.options,   'options');
 };
